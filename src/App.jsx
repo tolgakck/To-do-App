e62c557e98a3b065066ac44c2c7f141685ceb49c
ahmettolgakucuk <at_kucuk@hotmail.com>
2026-03-23 01:42:12 +0300

    UI changes setted.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './css/App.css';
 import TodoCreate from './components/TodoCreate';
 import TodoList from './components/TodoList';
 
 function App() {
   const [todos, setTodos] = useState([]);
-
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem('todos'));
+    if (savedTodos) setTodos(savedTodos);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
   const createTodo = (newTodo) => {
     setTodos([...todos, newTodo]);
   }
